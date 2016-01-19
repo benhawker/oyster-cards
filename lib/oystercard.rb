b7927124d71e7378286eecfc4f38434c52c0dd15
Ben <ben.c.hawker@gmail.com>
2016-01-19 21:30:03 +0800

    Cannot load more than 50

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -4,12 +4,19 @@ class Oystercard
 	MAX_BALANCE = 50
 
 	def initialize(balance=0)
+		raise "Max balance is #{Oystercard::MAX_BALANCE}" if exceeds_max_balance?(balance)
 		@balance = balance
 	end
 
 
 	def top_up(amount)
 		@balance += amount
+	end
+
+	private
+
+	def exceeds_max_balance?(balance)
+		true if balance > MAX_BALANCE
 	end
 
 end
