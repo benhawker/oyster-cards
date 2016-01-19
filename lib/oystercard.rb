b2d0cdc82c9938c605928973a75d31d85408ad48
Ben <ben.c.hawker@gmail.com>
2016-01-19 21:15:29 +0800

    Adds balance to intialize

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,8 +2,13 @@ class Oystercard
 
 	attr_reader :balance
 
-	def initialize
-		@balance = 0
+	def initialize(balance=0)
+		@balance = balance
+	end
+
+
+	def top_up(amount)
+
 	end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,8 +4,13 @@ describe Oystercard do
 
 	let(:oystercard) { Oystercard.new }
 
-	it "is created with a balance of zero" do
+	it "is created with a balance of zero by default" do
 		expect(oystercard.balance).to eq (0)
+	end
+
+	it "can be created with a balance" do
+		oystercard = Oystercard.new(20)
+		expect(oystercard.balance).to eq (20)
 	end
 
 end
