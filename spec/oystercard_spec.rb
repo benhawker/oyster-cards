06261a5fd028a98bd17fbf7a21098f9589c684d0
Ben <ben.c.hawker@gmail.com>
2016-01-19 21:07:54 +0800

    Created with a balance of zero

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,4 +2,10 @@ require 'oystercard'
 
 describe Oystercard do
 
+	let(:oystercard) { Oystercard.new }
+
+	it "is created with a balance of zero" do
+		expect(oystercard.balance).to eq (0)
+	end
+
 end
